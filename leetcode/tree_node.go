edb30551cca8d049a5da6a71137cc0390ee45fe5
Andrew Batallones <batallonesa@gmail.com>
2025-03-25 11:57:42 -0400

    Add additional helper methods for TreeNode

--- leetcode/tree_node.go
+++ leetcode/tree_node.go
@@ -1,7 +1,39 @@
 package leetcode
 
+import "fmt"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
+}
+
+func NewTree(nums []int) *TreeNode {
+	return insertNode(nums, 0)
+}
+
+func insertNode(nums []int, i int) *TreeNode {
+	if i < len(nums) {
+		root := &TreeNode{
+			Val: nums[i],
+		}
+
+		root.Left = insertNode(nums, 2*i+1)
+		root.Right = insertNode(nums, 2*i+2)
+
+		return root
+	}
+
+	return nil
+}
+
+func (tn *TreeNode) Print() {
+	if tn == nil {
+		fmt.Print("")
+	} else {
+		fmt.Printf("%d ", tn.Val)
+
+		tn.Left.Print()
+		tn.Right.Print()
+	}
 }
