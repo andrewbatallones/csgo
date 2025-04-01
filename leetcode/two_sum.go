f89e37e54a703ddae4927ed029f983005643c7d6
Andrew Batallones <batallonesa@gmail.com>
2025-04-01 13:55:36 -0400

    leetcode: Add OG 2sum

--- leetcode/two_sum.go
+++ leetcode/two_sum.go
@@ -1,8 +1,21 @@
 package leetcode
 
+// 1. Two Sum
+func TwoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return []int{0, 0}
+}
+
 // 167. Two Sum II - Input Array Is sorted
 // There will always be at least 2 numbers
-func TwoSum(numbers []int, target int) []int {
+func TwoSumTwo(numbers []int, target int) []int {
 	// We have 2 pointers (left, right)
 	// If the number is bigger, lower right
 	// If the number is smaller, increase left
